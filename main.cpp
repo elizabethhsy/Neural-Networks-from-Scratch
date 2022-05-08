c1e5b9fafa12bda166554c89ca7bea1185f6ed7e
Elizabeth <elizabethhsy12@gmail.com>
2022-05-08 23:48:26 +0100

    Softmax activation function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
 #include <functional>
 #include <iostream>
+#include <math.h>
 #include <numeric>
 #include <tuple>
 #include <vector>
@@ -34,8 +36,30 @@ struct Activation_ReLU {
     }
 };
 
+struct Activation_Softmax {
+    MatrixXd outputs;
+    void forward(MatrixXd inputs) {
+        VectorXd maximum = inputs.rowwise().maxCoeff();
+        // prevent overflow
+        inputs.colwise() -= maximum;
+
+        // exponentiate
+        MatrixXd output_layer = inputs.unaryExpr([](double i) {
+            return exp(i);
+        });
+
+        // normalize
+        VectorXd output_sum = output_layer.rowwise().sum();
+        MatrixXd normalized_output(output_layer.rows(), output_layer.cols()+1);
+        normalized_output << output_layer, output_sum;
+
+        outputs = normalized_output.rowwise().hnormalized();
+    }
+};
+
 int main(int, char**) {
-    Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+    srand((unsigned int) time(0));
+    Eigen::IOFormat CleanFmt(6, 0, ", ", "\n", "[", "]");
 
     // input data
     MatrixXd X(3, 4);
@@ -43,14 +67,18 @@ int main(int, char**) {
 
     Layer_Dense layer1(4, 5);
     Activation_ReLU activation1;
+    Activation_Softmax softmax1;
     Layer_Dense layer2(5, 2);
 
     layer1.forward(X);
     activation1.forward(layer1.outputs);
     layer2.forward(activation1.outputs);
+    softmax1.forward(layer2.outputs);
+
 
     std::cout << layer1.outputs.format(CleanFmt) << "\n";
     std::cout << activation1.outputs.format(CleanFmt) << "\n";
     std::cout << layer2.outputs.format(CleanFmt) << "\n";
+    std::cout << softmax1.outputs.format(CleanFmt) << "\n";
     return 0;
 }
