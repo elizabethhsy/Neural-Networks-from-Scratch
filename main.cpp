fcda051b3406c3b90f2328b534d1918fc7dd3c2a
Elizabeth <elizabethhsy12@gmail.com>
2022-05-09 17:09:47 +0100

    loss function calculation

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 #include <functional>
@@ -57,8 +58,47 @@ struct Activation_Softmax {
     }
 };
 
+struct Loss {
+    virtual VectorXd forward(MatrixXd outputs, MatrixXd y) = 0;
+
+    float calculate(MatrixXd outputs, MatrixXd y) { // y is the intended target values
+        VectorXd sample_losses(outputs.rows());
+        sample_losses = forward(outputs, y);
+
+        float data_loss = sample_losses.mean();
+        return data_loss;
+    }
+};
+
+// categorical cross entropy loss
+struct Loss_CategoricalCrossentropy : Loss {
+    VectorXd forward(MatrixXd y_pred, MatrixXd y_true) { // y_pred are the NN's predicted values, y_true are the target values
+        int samples = y_pred.rows(); // get the number of samples in a batch
+        MatrixXd y_pred_clipped = y_pred.unaryExpr([](double i) {
+            return std::clamp(i, 1e-7, 1-1e-7); // prevent log(0)
+        });
+
+        // check if the y_true values are passed in as scalar values or one hot encoded values
+        VectorXd correct_confidences(y_pred.rows());
+        if (y_true.rows() == 1) {// scalar values
+            for (int i=0; i<y_pred.rows(); i++) {
+                correct_confidences[i] = y_pred_clipped.coeff(i, y_true.coeff(0, i)); // ith row, index specified by y_true
+            }
+        }
+        else { // one hot encoded values
+            correct_confidences = (y_pred_clipped*y_true).rowwise().sum();
+        }
+
+        VectorXd negative_log_likelihoods = correct_confidences.unaryExpr([](double i) {
+            return -1*log(i);
+        });
+
+        return negative_log_likelihoods;
+    }
+};
+
 int main(int, char**) {
-    srand((unsigned int) time(0));
+    // srand((unsigned int) time(0));
     Eigen::IOFormat CleanFmt(6, 0, ", ", "\n", "[", "]");
 
     // input data
@@ -68,17 +108,32 @@ int main(int, char**) {
     Layer_Dense layer1(4, 5);
     Activation_ReLU activation1;
     Activation_Softmax softmax1;
-    Layer_Dense layer2(5, 2);
+    Layer_Dense layer2(5, 3);
+    Loss_CategoricalCrossentropy loss_function;
 
     layer1.forward(X);
     activation1.forward(layer1.outputs);
     layer2.forward(activation1.outputs);
     softmax1.forward(layer2.outputs);
 
+    VectorXd target_output(3);
+    target_output << 1, 0, 2;
 
-    std::cout << layer1.outputs.format(CleanFmt) << "\n";
-    std::cout << activation1.outputs.format(CleanFmt) << "\n";
-    std::cout << layer2.outputs.format(CleanFmt) << "\n";
+    // VectorXd loss(3);
+    // for (int i=0; i<target_output.size(); i++) {
+    //     // categorical cross entropy loss
+    //     loss[i] = -1*log(softmax1.outputs.coeff(target_output[i], i));
+    // }
+    // float loss_value = loss.mean();
+
+    float loss = loss_function.calculate(softmax1.outputs, target_output.matrix());
+
+    // std::cout << layer1.outputs.format(CleanFmt) << "\n";
+    // std::cout << activation1.outputs.format(CleanFmt) << "\n";
+    // std::cout << layer2.outputs.format(CleanFmt) << "\n";
     std::cout << softmax1.outputs.format(CleanFmt) << "\n";
+    // std::cout << loss.format(CleanFmt) << "\n";
+    // std::cout << loss_value << "\n";
+    std::cout << loss << "\n";
     return 0;
 }
